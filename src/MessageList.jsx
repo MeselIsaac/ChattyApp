d03a16df4b97b4ee370fffd3f9fd8cf61f32adfb
Mesel <meselisaac03@hotmail.com>
2019-04-09 22:06:35 +0000

    initial commit after completeing 'Component Lifecycle Methods' section

---
+++ src/MessageList.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+
+    render() {
+
+      const listMessages = this.props.messages.map(function(message) {
+        
+         return (<Message key = {message.id} message = {message}/>)
+      }); 
+        
+        
+      return (
+        <div id= "messages-container">
+        { listMessages }
+        </div>
+      );
+    }
+  }
+  export default MessageList;
+
+
+
+
+
