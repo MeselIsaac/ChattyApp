bde8e039b5dde26cddbaeeaa99c972a81b1a9926
Mesel <meselisaac03@hotmail.com>
2019-04-10 22:04:58 +0000

    added broadcast function to send data to react client

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const SocketServer = require('ws').Server;
+const WebSocket = require('ws')
+const SocketServer = WebSocket.Server;
 const uuidv4 = require('uuid/v4');
 
 // Set the port to 3001
@@ -13,6 +14,19 @@ const server = express()
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
+
+wss.broadcast = data => {
+  wss.clients.forEach(ws => {
+    console.log("RUnnin")
+    console.log(ws.readyState, WebSocket.OPEN)
+    
+    if (ws.readyState === WebSocket.OPEN) {
+      
+      console.log("INSIDE IF")
+      ws.send(data);
+    }
+  });
+};
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
@@ -28,11 +42,10 @@ wss.on('connection', (ws) => {
       username: messageObj.username
 
     }
-    console.log(messageToBroadcast)
+    console.log(JSON.stringify(messageToBroadcast))
+    wss.broadcast(JSON.stringify(messageToBroadcast));
   })
-  // this.socket.onmessage = function (event) {
-  //   console.log(event.data);
-  // }
+ 
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   // ws.on('close', () => console.log('Client disconnected'));
