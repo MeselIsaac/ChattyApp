a816132bc54e34410dc90559a4f882e24940fcc8
Mesel <meselisaac03@hotmail.com>
2019-04-12 00:11:29 +0000

    cleaned code up a little

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,45 +1,42 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
- 
- 
-  onUsernameSubmit = (e) => {
-    if (e.keyCode === 13) {
-      const usernameObj = {
-        type: "notificationToServer",
-        username: e.target.value
-      }
-      this.props.sendMessageToServer(usernameObj)
-    }
-  }
-
-  onMessageSubmit = (e) => {
-    if (e.keyCode === 13) {
-      const messageObj = {
-        type: "messageToServer",
-        message: e.target.value
-      }
-      this.props.sendMessageToServer(messageObj)
-      e.target.value = ""
-    }
-  }
-
-    render() {
   
+  render() {
+    
+    return (
+      <footer className="chatbar">
+        <input className="chatbar-username" defaultValue= {this.props.currentUser} onKeyDown = {this.onUsernameSubmit} placeholder="Your Name (Optional)" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER " onKeyDown = {this.onMessageSubmit} />
+      </footer>
       
-      return (
-        <footer className="chatbar">
-        
-        
-            <input className="chatbar-username" defaultValue= {this.props.currentUser} onKeyDown = {this.onUsernameSubmit} placeholder="Your Name (Optional)" />
-            <input className="chatbar-message" placeholder="Type a message and hit ENTER " onKeyDown = {this.onMessageSubmit} />
-           
-      
-        
-        </footer>
-
       );
     }
-  }
 
-  export default ChatBar;
+    //Event handler that sends username and adds type of message up to App.jsx on enter
+    onUsernameSubmit = (e) => {
+      //On Enter
+      if (e.keyCode === 13) {
+        const usernameObj = {
+          type: "notificationToServer",
+          username: e.target.value
+        }
+        this.props.sendMessageToServer(usernameObj)
+      }
+    }
+  
+    //Event handler that sends message and adds type of message up to App.jsx on enter
+    onMessageSubmit = (e) => {
+      //On Enter
+      if (e.keyCode === 13) {
+        const messageObj = {
+          type: "messageToServer",
+          message: e.target.value
+        }
+        this.props.sendMessageToServer(messageObj)
+        e.target.value = ""
+      }
+    }
+}
+
+export default ChatBar;
